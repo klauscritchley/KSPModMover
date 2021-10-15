6898ec8a8b183567bb6a5c370b15b987c82be2d7
klauscritchley <38118669+klauscritchley@users.noreply.github.com>
2021-10-15 17:31:46 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.status1 = new System.Windows.Forms.Label();
+            this.revertDS = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(121, 231);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(136, 26);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Remove mods";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // status1
+            // 
+            this.status1.AutoSize = true;
+            this.status1.Location = new System.Drawing.Point(280, 308);
+            this.status1.Name = "status1";
+            this.status1.Size = new System.Drawing.Size(90, 13);
+            this.status1.TabIndex = 1;
+            this.status1.Text = "Waiting for start...";
+            // 
+            // revertDS
+            // 
+            this.revertDS.Location = new System.Drawing.Point(375, 231);
+            this.revertDS.Name = "revertDS";
+            this.revertDS.Size = new System.Drawing.Size(133, 26);
+            this.revertDS.TabIndex = 2;
+            this.revertDS.Text = "Add mods back";
+            this.revertDS.UseVisualStyleBackColor = true;
+            this.revertDS.Click += new System.EventHandler(this.revertDS_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(151, 365);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 31);
+            this.errorLabel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(578, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "This app does NOT delete your mods, it just moves them out of the GameData so KSP" +
+    " doens\'t load them at game startup.\r\nWorks in just seconds, even on a old hard d" +
+    "rive!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 515);
+            this.Controls.Add(this.btnClickThis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.revertDS);
+            this.Controls.Add(this.status1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label status1;
+        private System.Windows.Forms.Button revertDS;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public int timess = 0;
+        public string ec = "Error code: ";
+        public bool rStatus;
+
+        public string mPath = @"D:\Steam\steamapps\common\Kerbal Space Program\";
+        void btnClickThis_Click(object sender, EventArgs e)
+        {
+            status1.Text = "Starting process...";
+            string sourceDirName = mPath + "GameData";
+            string destDirName = mPath + "Mods NOT IN USE";
+            movee(sourceDirName, destDirName, "AA1");
+            Directory.CreateDirectory(mPath + "GameData");
+            sourceDirName = mPath + "Mods NOT IN USE/Squad";
+            destDirName = mPath + "GameData/Squad";
+            movee(sourceDirName, destDirName, "AA2");
+            sourceDirName = mPath + "Mods NOT IN USE/SquadExpansion";
+            destDirName = mPath + "GameData/SquadExpansion";
+            movee(sourceDirName, destDirName, "AA3");
+            if (rStatus == true)
+            {
+                status1.Text = "Mods successfully moved, game will launch fully stock!";
+            }
+            else
+            {
+                status1.Text = "Error detected when removing mods, if you accidently hit the button twice, that explains why.";
+            }
+
+        }
+        void revertDS_Click(object sender, EventArgs e)
+        {
+
+            string sw;
+            string dw; 
+            sw = mPath + "GameData/SquadExpansion";
+            dw = mPath + "Mods NOT IN USE/SquadExpansion";
+
+            movee(sw, dw, "AB2");
+            sw = mPath + "GameData/Squad";
+            dw = mPath + "Mods NOT IN USE/Squad";
+            movee(sw, dw, "AB1");
+            try
+            {
+                Directory.Delete(@"D:\Steam\steamapps\common\Kerbal Space Program\GameData");
+            }
+            catch
+            {
+                errorLabel.Text = "ugh this again";
+            } 
+            string dsw = mPath + "Mods NOT IN USE";
+            string ddw = mPath + "GameData";
+            try
+            {
+                Directory.Move(dsw, ddw);
+            }
+            catch
+            {
+                
+            }
+            if (rStatus == true)
+            {
+                status1.Text = "Mods successfully moved back!";
+            }
+            else
+            {
+                status1.Text = "Error detected when moving mods back.";
+            }
+
+        }
+        void movee(string sourceDirName, string destDirName, string errorCode)
+        {
+            try
+            {
+                Directory.Move(sourceDirName, destDirName);
+                rStatus = true;
+                errorLabel.Text = "";
+            }
+            catch (IOException exp)
+            {
+                //string moveError =  (exp.Message);
+                Console.WriteLine("test");
+                errorLabel.Text = ec + errorCode;
+                rStatus = false;
+            }
+            timess++;
+            string ttimes = timess.ToString();
+            //status1.Text = ttimes;
+        }
+        public void yoyoyo(object sender, EventArgs e)
+        { 
+            try
+            {
+                string[] dirs = Directory.GetDirectories(@"c:\", "p*", SearchOption.TopDirectoryOnly);
+                Console.WriteLine("The number of directories starting with p is {0}.", dirs.Length);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+            }
+            catch 
+            {
+
+            }
+        }
+
+    }
+        
+    }
+
